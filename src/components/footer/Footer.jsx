baaa91136baab03f266e3b8476a6e7147ef848cf
sri766 <srisanthseth28@gmail.com>
2023-12-29 19:55:47 +0530

    version2 added p tag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,14 +6,8 @@ import { IoMdHeart } from "react-icons/io";
 const Footer = ({mode}) => {
   return (
     <div className={`footer ${mode === 'light' ? 'light-mode' : ''}`}>
-      {/* <h1>Let's Connect</h1>
-      <ul className='footer_contents'>
-        <li className='content'>Twitter</li>
-        <li className='content'>Facebook</li>
-        <li className='content'>Instagram</li>
-        <li className='content'>LinkedIn</li>
-      </ul> */}
       <p>Made with {<IoMdHeart />} by Srisanth Seth</p>
+      <p>Version 2</p>
     </div>
   )
 }
